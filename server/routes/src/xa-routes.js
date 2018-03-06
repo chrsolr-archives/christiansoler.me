04c0670b9d79ccf5f6a77e0fac3a0f63d23021a9
Christian Soler <chr.solr@gmail.com>
2018-03-05 19:56:23 -0600

    updated

--- server/routes/src/xa-routes.js
+++ server/routes/src/xa-routes.js
@@ -12,7 +12,7 @@ module.exports = () => {
     // Home route
     router.get('/android/xa/version', (req, res, next) => {
         return res.status(200).send({
-            version_code: 1,
+            version_code: 2,
             version_name: '0.0.1',
             xda_thread_url: 'https://forum.xda-developers.com/android/apps-games/app-xa-unofficial-xboxachievements-com-t3600450',
             download_url: 'https://labs.xda-developers.com/store/app/io.keypunchers.xa'
